bdd40b2b05bfd70aa31bd298fb93b14e19304465
Berkcan Telseren <berkcantelseren@gmail.com>
2025-01-25 22:54:38 +0300

    main form create

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,14 @@
 export default function Main() {
   return (
     <main>
-      <input type="text" />
+      <form className="ingredient-form">
+        <input
+          aria-label="Add ingredient"
+          type="text"
+          placeholder="e.g. Paprika"
+        />
+        <button>Add ingredient</button>
+      </form>
     </main>
   );
 }
