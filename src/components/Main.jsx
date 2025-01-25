69828a8aec70e3a5c43cd7f31336af604d2a05b7
Berkcan Telseren <berkcantelseren@gmail.com>
2025-01-25 23:40:52 +0300

    form create and using handleSubmit to get new data to array

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,30 @@
 export default function Main() {
+  const ingredients = ["Chicken", "Tomatoes", "Cucember"];
+
+  const ingredientsListItems = ingredients.map((ingredient) => (
+    <li key={ingredient}>{ingredient}</li>
+  ));
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formInputs = new FormData(event.currentTarget);
+    const newIngredient = formInputs.get("ingredient");
+    console.log(newIngredient);
+  }
+
   return (
     <main>
-      <form className="ingredient-form">
+      <form onSubmit={handleSubmit} className="ingredient-form">
         <input
           aria-label="Add ingredient"
           type="text"
           placeholder="e.g. Paprika"
+          name="ingredient"
         />
         <button>Add ingredient</button>
       </form>
+
+      <ul>{ingredientsListItems}</ul>
     </main>
   );
 }
