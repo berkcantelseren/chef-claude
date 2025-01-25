e684748be92275b2a13f4561e73a7baed57f05c3
Berkcan Telseren <berkcantelseren@gmail.com>
2025-01-25 22:50:31 +0300

    main component create and change main to index for solving naming issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import Header from "./components/Header.jsx";
+import Main from "./components/Main.jsx";
 import "./App.css";
 
 function App() {
   return (
     <>
       <Header />
+      <Main />
     </>
   );
 }
